471b76cd6378d35da15fb6e312743b5bd9ba9468
Hugo <hugoshi@blob.(none)>
2012-01-23 17:42:22 -0500

    made example directory

--- examples/pyzmq_rep_server.py
+++ examples/pyzmq_rep_server.py
@@ -2,11 +2,11 @@ import zmq
 import time
 
 c = zmq.Context()
-s = c.socket(zmq.REP)
+s = c.socket(zmq.XREP)
 s.bind('tcp://127.0.0.1:10001')
 
 while True:
     msg = s.recv_multipart()
     print 'received', msg
-    s.send_multipart(['goober'])
+    s.send_multipart(msg)
     
